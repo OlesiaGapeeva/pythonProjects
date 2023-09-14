561ec91fb508ef6062eb3092598d6a885515136b
OlesiaGapeeva <olgap03@gmail.com>
2023-09-14 11:22:23 +0000

    function GetData

--- bmstu_lab/bmstu_lab1/views.py
+++ bmstu_lab/bmstu_lab1/views.py
@@ -7,7 +7,22 @@ def hello(request):
         'list': ['python', 'django', 'html']
     }})
 
+def GetData(id):
+    data = [
+            {'title': 'Копирайтер', 'salary': 50000, 'company': 'Копи-копи', 'city': 'Москва', 'exp': 'Без опыта', 'image': "https://w7.pngwing.com/pngs/299/589/png-transparent-social-media-computer-icons-technical-computer-network-text-computer.png", 'id': 1},
+            {'title': 'Менеджер блогера', 'salary': 10000, 'company': 'Co_blog', 'city': 'Москва','exp': 'Без опыта','image' :None,'id': 2},
+            {'title': 'Куратор', 'salary': 7000, 'company': 'Вебскул', 'city': 'Без города','exp': 'Без опыта', 'id': 3},
+        ]
+    if id == -1:
+        return data
+    return data[id - 1]
+
 def GetOrders(request):
+    return render(request, 'orders.html',{'data' : {
+        'current_date': date.today(),
+        'orders': GetData(-1)}})
+
+# def GetOrders(request):
     return render(request, 'orders.html', {'data' : {
         'current_date': date.today(),
         'orders': [
