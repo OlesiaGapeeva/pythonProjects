59f4dd48d9325e59c5047859d71bd93a68551acb
OlesiaGapeeva <olgap03@gmail.com>
2023-09-18 20:24:58 +0000

    lab 2

--- bmstu_lab/bmstu_lab/urls.py
+++ bmstu_lab/bmstu_lab/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from bmstu_lab1 import views
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
