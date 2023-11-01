b945e09bede07029b50df1fd36d2d6b4047be2fb
OlesiaGapeeva <olgap03@gmail.com>
2023-11-01 21:34:28 +0000

    first commit

---
+++ my-app/src/main.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ITunesPage from './ITunesPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Это наша стартовая страница</h1>
+  },
+  {
+    path: '/new',
+    element: <h1>Это наша страница с чем-то новеньким</h1>
+  },
+  {
+    path: '/test',
+    element: <ITunesPage />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ul>
+      <li>
+        <a href="/">Старт</a>
+      </li>
+      <li>
+        <a href="/new">Хочу на страницу с чем-то новеньким</a>
+      </li>
+    </ul>
+    <hr />
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
