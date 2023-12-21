829b589797b4e920e8b0a6942b70d0aa90f1adc4
OlesiaGapeeva <92272887+OlesiaGapeeva@users.noreply.github.com>
2023-12-21 18:56:25 +0300

    Delete bmstu_lab directory

--- bmstu_lab/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from myapp.models import *
-
-admin.site.register(Vacancies)
-admin.site.register(Responses)
-admin.site.register(ResponsesVacancies)
-admin.site.register(Users)
-# Register your models here.
